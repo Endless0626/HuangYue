4c6461903ae1d00e9df93dc9967cabf9a015cff9
Endless0626 <2024743047@qq.com>
2020-05-15 22:45:09 +0800

    0515 update a/b add a new file monky

--- a除b.py
+++ a除b.py
@@ -1,5 +1,5 @@
-a,b=input().split(";")#同行输入，以“；”分隔
+a,b=input().split(" ")#同行输入，以“ ”分隔
 if eval(b)==0:
-    print("除零错误")
+    print("出现了错误：除零错误")
 else:
-    print("{:.2f}".format(eval(a)/eval(b)))
+    print("得到的结果是：{:.2f}".format(eval(a)/eval(b)))
---
+++ 猴子吃桃.py
@@ -0,0 +1,5 @@
+def f(n):
+    for i in range(1,10):
+        n=(n+1)*2
+    return n
+print(f(1))
